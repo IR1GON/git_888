95d569579a6af7d5ad9719a4bd036631700e3de9
Pic <nik20010106@gmail.com>
2023-11-29 17:29:11 +0200

    HW_004 done

--- Dockerfile
+++ Dockerfile
@@ -1,13 +1,6 @@
 FROM python:3.11.1
-
 WORKDIR /app
 
-ENV PYTHONUNBUFFERED 1
-ENV PYTHONDONTWRITEBYTECODE 1
-
 COPY . .
-
-RUN echo "Hello from installing"
-RUN apt-get upgrade
 
 CMD ["python", "main.py"]
--- main.py
+++ main.py
@@ -1,31 +1,24 @@
-import requests
-import csv
+import sys
 import time
+#start in the terminal - 'python main.py 10(number of seconds)'
+def print_time(seconds):
+    for i in range(seconds):
+        print(f"Time: {i + 1} second(s)")
+        time.sleep(1)
 
-def get_iss_position():
-    response = requests.get('http://api.open-notify.org/iss-now.json')
-    if response.status_code == 200:
-        data = response.json()
-        timestamp = data['timestamp']
-        latitude = data['iss_position']['latitude']
-        longitude = data['iss_position']['longitude']
-        return timestamp, latitude, longitude
-    else:
-        print("Failed to fetch ISS position")
-        return None, None, None
+def main():
+    if len(sys.argv) != 2:
+        print("Please provide only one argument - an integer representing seconds")
+        return
 
-def write_to_csv(file_path, data):
-    with open(file_path, mode='a', newline='') as file:
-        writer = csv.writer(file, delimiter=';')
-        writer.writerow(data)
+    try:
+        seconds = int(sys.argv[1])
+        if seconds <= 0:
+            print("Please enter a positive integer")
+            return
+        print_time(seconds)
+    except ValueError:
+        print("Incorrect number format. Please enter an integer.")
 
 if __name__ == "__main__":
-    csv_file = 'iss_positions.csv'
-
-    while True:
-        timestamp, latitude, longitude = get_iss_position()
-        if timestamp and latitude and longitude:
-            data_to_write = [timestamp, latitude, longitude]
-            write_to_csv(csv_file, data_to_write)
-            print(f"Position recorded: Timestamp - {timestamp}, Latitude - {latitude}, Longitude - {longitude}")
-        time.sleep(5)
+    main()
